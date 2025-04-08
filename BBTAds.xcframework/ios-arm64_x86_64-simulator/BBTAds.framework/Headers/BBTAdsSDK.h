49ce0f8115ec423a469d4e810d6618b451eefdbb
nanxia <zsai@tsiannian.com>
2025-04-08 14:30:15 +0800

    add framework

---
+++ BBTAds.xcframework/ios-arm64_x86_64-simulator/BBTAds.framework/Headers/BBTAdsSDK.h
@@ -0,0 +1,37 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BBTAdsSDK : NSObject
+
+/// SDK 版本号
++ (NSString *)version;
+
+/// 注册 SDK
+/// @param appId 应用ID
+/// @param mediaId 媒体ID 
++ (void)registerWithAppId:(NSString *)appId mediaId:(NSString *)mediaId secret:(NSString *)secret;
+
+/// 是否已经初始化
++ (BOOL)isInitialized;
+
+/// 设置测试模式
+/// @param isTest 是否为测试模式
++ (void)setTestMode:(BOOL)isTest;
+
+/// 获取当前测试模式状态
++ (BOOL)isTestMode;
+
+/// 设置日志开关
+/// @param enabled 是否开启日志
++ (void)setLogEnabled:(BOOL)enabled;
+
+/// 上报错误日志
+/// @param errorSummary 错误摘要
+/// @param errorDetails 错误详情
++ (void)reportError:(nullable NSString *)errorSummary
+       errorDetails:(nullable NSString *)errorDetails;
+
+@end
+
+NS_ASSUME_NONNULL_END
